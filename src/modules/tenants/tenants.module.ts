f041d19f7c3672020681b640b10441f3c7852411
Grey Boolean <greyboolean@gmail.com>
2024-05-22 16:19:09 +0530

    resolve missing dependencies

--- src/modules/tenants/tenants.module.ts
+++ src/modules/tenants/tenants.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TenantsService } from './tenants.service';
 import { TenantsController } from './tenants.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [TenantsController],
   providers: [TenantsService],
 })
