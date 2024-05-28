8994e29d59e33d385702017b79168b22b6e8444f
Grey Boolean <greyboolean@gmail.com>
2024-05-28 11:40:19 +0530

    override tenant prisma methods to filter by tenanId

--- src/modules/prisma/tenant-prisma.service.ts
+++ src/modules/prisma/tenant-prisma.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/tenant-client';
+import { PrismaClient, Prisma } from '@prisma/tenant-client';
+
+interface IData {
+  data: any;
+}
 
 @Injectable()
 export class TenantPrismaService extends PrismaClient {
@@ -10,8 +14,38 @@ export class TenantPrismaService extends PrismaClient {
   extend(tenantId: string) {
     return this.$extends({
       query: {
-        $allOperations({ query }) {
-          return query({ where: { tenantId } });
+        $allModels: {
+          // async create({ args, query }) {
+          //   return query({ data: { ...args.data, tenantId } });
+          // },
+          async create<T, A extends IData>(
+            this: T,
+            args: Prisma.Exact<
+              A,
+              Prisma.Args<T, 'create'> & { data: Omit<A['data'], 'tenantId'> }
+            >,
+          ): Promise<Prisma.Result<T, A, 'create'>> {
+            const context = Prisma.getExtensionContext(this);
+            console.log(context);
+            return (context as any).create({
+              data: { ...args.data, tenantId },
+            });
+          },
+          async findMany({ args, query }) {
+            return query({ ...args, where: { tenantId } });
+          },
+          async findUnique({ args, query }) {
+            return query({ where: { ...args.where, tenantId } });
+          },
+          async update({ args, query }) {
+            return query({
+              where: { ...args.where, tenantId },
+              data: args.data,
+            });
+          },
+          async delete({ args, query }) {
+            return query({ where: { ...args.where, tenantId } });
+          },
         },
       },
     });
