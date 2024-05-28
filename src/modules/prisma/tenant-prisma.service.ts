f4d1f5915dbc7282e734da46f2ceb77c754400f8
Grey Boolean <greyboolean@gmail.com>
2024-05-28 11:40:59 +0530

    remove type utitilities from tenant prisma client

--- src/modules/prisma/tenant-prisma.service.ts
+++ src/modules/prisma/tenant-prisma.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient, Prisma } from '@prisma/tenant-client';
+import { PrismaClient } from '@prisma/tenant-client';
+// import { PrismaClient, Prisma } from '@prisma/tenant-client';
 
-interface IData {
-  data: any;
-}
+// interface IData {
+//   data: any;
+// }
 
 @Injectable()
 export class TenantPrismaService extends PrismaClient {
@@ -15,22 +16,23 @@ export class TenantPrismaService extends PrismaClient {
     return this.$extends({
       query: {
         $allModels: {
-          // async create({ args, query }) {
-          //   return query({ data: { ...args.data, tenantId } });
-          // },
-          async create<T, A extends IData>(
-            this: T,
-            args: Prisma.Exact<
-              A,
-              Prisma.Args<T, 'create'> & { data: Omit<A['data'], 'tenantId'> }
-            >,
-          ): Promise<Prisma.Result<T, A, 'create'>> {
-            const context = Prisma.getExtensionContext(this);
-            console.log(context);
-            return (context as any).create({
-              data: { ...args.data, tenantId },
-            });
+          async create({ args, query }) {
+            return query({ data: { ...args.data, tenantId } });
           },
+          // TODO implement type-safe create method
+          // async create<T, A extends IData>(
+          //   this: T,
+          //   args: Prisma.Exact<
+          //     A,
+          //     Prisma.Args<T, 'create'> & { data: Omit<A['data'], 'tenantId'> }
+          //   >,
+          // ): Promise<Prisma.Result<T, A, 'create'>> {
+          //   const context = Prisma.getExtensionContext(this);
+          //   console.log(context);
+          //   return (context as any).create({
+          //     data: { ...args.data, tenantId },
+          //   });
+          // },
           async findMany({ args, query }) {
             return query({ ...args, where: { tenantId } });
           },
