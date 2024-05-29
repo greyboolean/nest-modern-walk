b91c7fc0283ac27611358a064aaa60a1529dbe50
Grey Boolean <greyboolean@gmail.com>
2024-05-29 09:57:34 +0530

    remove relflector log

--- src/modules/auth/guards/auth.guard.ts
+++ src/modules/auth/guards/auth.guard.ts
@@ -27,7 +27,6 @@ export class AuthGuard implements CanActivate {
   private usersService: UsersService;
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    console.log(this.reflector);
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
       context.getClass(),
