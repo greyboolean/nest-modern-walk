4a1be8a9549e3be57dc625ab80d880530cca0d7b
Grey Boolean <greyboolean@gmail.com>
2024-05-30 13:54:25 +0530

    return users as user entitiy types for serialization to work

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,8 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Role } from '../enums/role.enum';
+import { Role, User as UserModel } from '@prisma/tenant-client';
+// import { Role } from '../enums/role.enum';
 import { Exclude } from 'class-transformer';
 
-export class User {
+export class User implements UserModel {
+  constructor(partial: Partial<User>) {
+    Object.assign(this, partial);
+  }
+
   @ApiProperty()
   id: number;
 
@@ -16,20 +21,45 @@ export class User {
   password: string;
 
   @ApiProperty()
-  name: { firstname: string; lastname: string };
+  name: {
+    id: number;
+    firstname: string;
+    lastname: string;
+  };
+
+  @Exclude()
+  nameId: number;
 
   @ApiProperty()
   address: {
+    id: number;
     city: string;
     street: string;
     number: number;
     zipcode: string;
-    geolocation: { lat: string; long: string };
+    geolocation: {
+      id: number;
+      lat: string;
+      long: string;
+    };
+    geolocationId: number;
   };
+
+  @Exclude()
+  addressId: number;
 
   @ApiProperty()
   phone: string;
 
   @ApiProperty()
   roles: Role[];
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+
+  @ApiProperty()
+  tenantId: string;
 }
